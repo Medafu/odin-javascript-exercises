34daab94cab1f79b4eb9d1fb41cc7473849b8705
Medafu <justrayzzin@gmail.com>
2022-07-05 22:27:08 +0300

    Finished 02 task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let save_string = string
+    if (number == 0) {
+        return ''
+    } else if (number < 0) {
+        return 'ERROR'
+    } else if (string == '') {
+        return string
+    }
+    
+    for (let i = 1; i < number; i++) {
+        string += save_string
+    }
+    return string
 };
 
 // Do not edit below this line
