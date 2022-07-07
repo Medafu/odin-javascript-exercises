1befdf5fdc39bf6bd2ce3735d84e9e610c457dd5
Medafu <justrayzzin@gmail.com>
2022-07-07 17:14:39 +0300

    Finished 05 task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof num1 !== "number" || typeof num2 !== "number") || (num1 < 0 || num2 < 0)) {
+        return "ERROR"
+    }
 
+    if (num1 > num2) {
+        let temp_num1 = num1
+        num1 = num2
+        num2 = temp_num1
+    }
+
+    let result = 0
+    for (;num1 <= num2; num1++)  {
+        result += num1
+    }
+
+    return result
 };
 
 // Do not edit below this line
