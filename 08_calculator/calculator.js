be9a03e73b51314a2008da85bc28dc494aa11783
Medafu <justrayzzin@gmail.com>
2022-07-11 22:53:48 +0300

    Finished 08 task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
+}
+
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const subtract = function() {
-	
+const sum = function(...args) {
+  let result = 0
+  let array = args[0]
+  array.forEach((e) => {
+    result += e
+  })
+  return result
 };
 
-const sum = function() {
-	
+const multiply = function(...args) {
+  let result = 1
+  let array = args[0]
+  array.forEach((e) => {
+    result = result * e
+  })
+  return result
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+	let result = 1
+ 
+  if (num == 0) {
+    return 1
+  }
 
-const factorial = function() {
-	
+  for (; num > 1; num--) {
+    result = result * num
+  }
+  return result
 };
 
 // Do not edit below this line
