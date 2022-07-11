e2831d9456466cee1b66129945e00782720cff49
Medafu <justrayzzin@gmail.com>
2022-07-12 00:55:14 +0300

    Finished 10 task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 1
+    let b = 1
 
+    num = parseInt(num)
+
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    for (let i = 2; i <= num - 1; i++) {
+        let temp_b = b
+        b = a + b
+        a = temp_b
+    }
+    return b
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(6))
