bf9356c7e62a97889b439a613d149fdf3758b51e
Medafu <justrayzzin@gmail.com>
2022-07-14 14:28:33 +0300

    Finished 12 task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    ages = []
+    let currentYear = new Date().getFullYear()
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            let age = currentYear - people[i].yearOfBirth
+            ages.push(age)
+            continue  
+        }
+
+        let age = people[i].yearOfDeath - people[i].yearOfBirth
+        ages.push(age)
+    }
+    
+    let highest = 0
+    let index = null
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > highest) {
+            highest = ages[i]
+            index = i
+        }
+    }
+
+    return people[index]
 
 };
 
